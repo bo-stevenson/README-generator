8bc2aac12c7b163e9893b4c84ae6d9c7fdb50f0e
Bo Stevenson <bostevenson1027@gmail.com>
2020-12-03 14:49:21 -0600

    installed inquirer

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+const inquirer = require("inquirer");
+
+const fs = require("fs");
+
+
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        message: "What would you like the title to be?",
+        name: "title",
+    },
 
+    {
+        type: "input",
+        message: ""
+    }
 ];
 
 // function to write README file
