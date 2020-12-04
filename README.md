c867c178129d451f9fc57697feb888a0f4a278e4
Bo Stevenson <bostevenson1027@gmail.com>
2020-12-03 19:35:02 -0600

    added markdown

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# README-generator
+# title
+
+## Overview
+    description
+
+## Table of Contents
+
+    1. [Installation](#installation)
+
+    2. [Usage](#usage)
+
+    3. [Contributions](#contribution)
+
+    4. [Test Instructions](#test-instructions)
+
+    5. [Screenshots](#screenshots)
+
+    6. [Licenses](#licenses)
+
+    7. [Questions + Contact Me](#questions)
+
+
+## Installation
+
+    install it
+
+## Useage
+
+    use it
+
+## Contribution
+
+    none
+
+## Test Instructions
+
+    undefined
+
+## Screenshots
+
+    Additional images of the code and product listed below.
+
+    ![Screenshot]
+## Licenses
+
+    [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+    
+## Questions?
+
+    My GitHub is [bo ](https://www.github.com/bo ).
+
+    My email is [bo@gmail](mailto:bo@gmail).
+  
+
+  
--- index.js
+++ index.js
@@ -25,7 +25,14 @@ const questions = [
 
     {
         type: "input",
-        message: "Any contributions to note?",
+        message: "Provide brief explanation on usage",
+        name: "usage",
+    },
+
+
+    {
+        type: "input",
+        message: "Enter Contribution guidelines: ",
         name: "contribution",
     },
 
@@ -55,38 +62,100 @@ const questions = [
     },
 ];
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
 // function to initialize program
 function init() {
+    
     inquirer.prompt(questions).then(data => {
         let fileName = "README.md";
-        let license = "";
+        let licenses = "";
 
         if (data.license === "MIT") {
-            license = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+            licenses = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
         }
 
         if (data.license === "ISC") {
-            license = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
+            licenses = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
         }
 
         if (data.license === "GNU") {
-            license = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+            licenses = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
         }
-    })
+    
+    let markdown = `# ${data.title}\n
+## Overview
+    ${data.description}\n
+## Table of Contents\n
+    1. [Installation](#installation)\n
+    2. [Usage](#usage)\n
+    3. [Contributions](#contribution)\n
+    4. [Test Instructions](#test-instructions)\n
+    5. [Screenshots](#screenshots)\n
+    6. [Licenses](#licenses)\n
+    7. [Questions + Contact Me](#questions)\n
+
+## Installation\n
+    ${data.installation}\n
+## Useage\n
+    ${data.usage}\n
+## Contribution\n
+    ${data.contribution}\n
+## Test Instructions\n
+    ${data.testInstructions}\n
+## Screenshots\n
+    Additional images of the code and product listed below.\n
+    ![Screenshot]
+## Licenses\n
+    ${licenses}\n    
+## Questions?\n
+    My GitHub is [${data.github}](https://www.github.com/${data.github}).\n
+    My email is [${data.email}](mailto:${data.email}).
+  
+
+  `;
+    fs.writeFile(fileName, markdown, (error) =>
+    error ? console.log("error") : console.log ("Success!"));
+     });
 }
+    
+    
+   
+    
 
 // function to generate markdown for README
 function generateMarkdown(data) {
-    return `# ${data.title}
+    `# ${data.title}\n
+## Overview
+    ${data.description}\n
+## Table of Contents\n
+    1. [Installation](#installation)\n
+    2. [Usage](#usage)\n
+    3. [Contributions](#contribution)\n
+    4. [Test Instructions](#test-instructions)\n
+    5. [Screenshots](#screenshots)\n
+    6. [Licenses](#licenses)\n
+    7. [Questions + Contact Me](#questions)\n
+## Licenses\n
+    ${licenses}\n
+## Installation\n
+    ${data.installation}\n
+## Useage\n
+    ${data.usage}\n
+## Contribution\n
+    ${data.contribution}\n
+## Test Instructions\n
+    ${data.testInstructions}\n
+## Screenshots\n
+    Additional images of the code and product listed below.\n
+    ![Screenshot]
+## Questions?\n
+    My GitHub is [${data.github}](https://www.github.com/${data.github}).\n
+    My email is [${data.email}](mailto:${data.email}).
   
+
   `;
   }
+
   
-  module.exports = generateMarkdown;
   
 
 // function call to initialize program
